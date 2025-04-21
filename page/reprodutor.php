ce9d9828af653dd5f45aed87844f990177c5de0f
Rynilan <ilanvitorsantos@gmail.com>
2025-04-21 11:20:52 -0300

    feat: criada tela de reprodução e redirecionamento.
    Signed-off-by: Rynilan <ilanvitorsantos@gmail.com>

--- page/reprodutor.php
+++ page/reprodutor.php
@@ -1,3 +1,17 @@
 <?php 
-echo '<h1>Não implementado ainda.</h1>';
+session_start();
+include '../config/path.php';
+
+if ($_SESSION['autenticado'] || true) {
+	$conteudo = file_get_contents(ROOT_PATH.'html/reprodutor.html');
+	$custom_css = [
+		['path' => 'css/reprodutor.css'],
+	];
+	$custom_js = [
+		['path' => 'js/loadMediaInfo.js'],
+	];
+} else {
+	$conteudo = file_get_contents(ROOT_PATH.'html/40X/401.html');
+}
+include '../base/template.php';
 ?>
