d919a651dd43ae6bb4b0980b02c1f18f13b04c46
Rynilan <ilanvitorsantos@gmail.com>
2025-04-21 11:17:25 -0300

    cleanup: tirado código morto
    Signed-off-by: Rynilan <ilanvitorsantos@gmail.com>

--- page/login.php
+++ page/login.php
@@ -2,10 +2,9 @@
 if (!defined('ROOT_PATH')) {
 	include '../config/path.php';
 }
+
 $conteudo = file_get_contents(ROOT_PATH.'html/login.html');
-if (!$conteudo) {
-	unset($conteudo);
-}
+
 $custom_css = [['path' => 'css/form.css']];
 $custom_js = [['path' => 'js/login.js']];
 include ROOT_PATH.'base/template.php'; 
