cba5b22cf4bdf89ad9069c60afe25d043b5a7469
Rynilan <ilanvitorsantos@gmail.com>
2025-08-24 02:36:54 -0300

    code: ajustada o carregamento de séries.
    Signed-off-by: Rynilan <ilanvitorsantos@gmail.com>

--- db/getMidiaInfo.php
+++ db/getMidiaInfo.php
@@ -4,31 +4,50 @@ include 'conexao.php';
 if ($tipo === 'filme') {
     $sql = "select nome, sinopse, duracao, link from tb_".$tipo."s where id_".$tipo." = ".$id.";";
 } else {
-    $sql = "SELECT 
+	$sql = "SELECT 
             s.nome AS nome,
-            s.descricao AS sinopse,
-            MAX(e.temporada) AS duracao,
+            s.sinopse AS sinopse,
+            e.temporada AS duracao,
+            e.numero AS numero,
             e.link AS link
-          FROM 
-            tb_series s
-          JOIN 
-            tb_episodios e ON s.id_serie = e.id_serie
-          WHERE 
-            s.id_serie = ".$id.";";
+        FROM 
+            tb_series AS s
+        JOIN 
+            tb_episodios AS e ON s.id_serie = e.id_serie
+        WHERE 
+            s.id_serie = ".$id."
+        ORDER BY 
+            e.numero ASC;";
 }
 
-
 $query = $conexao->query($sql);
-$media_data = $query->fetch_assoc();
-$media_data['links'] = [];
+$media_data = [];
+$media_data['medias'] = [];
 $media_data['tipo'] = $tipo;
-foreach ($media_data as $key => $value) {
-    if (gettype($value) === 'string') {
-        if (strpos($value, 'https') === 0) {
-            array_push($media_data['links'], $value);
-            unset($media_data[$key]);
-        }
-    }
+
+if ($tipo === 'filme') {
+	$media_data = $query->fetch_assoc();
+	foreach ($media_data as $key => $value) {
+		if (gettype($value) === 'string') {
+			if (strpos($value, 'https') === 0) {
+				$media_data['medias'][] = [$value, 1, 0];
+				unset($media_data[$key]);
+			}
+		}
+	}
+} else {
+	while ($row = $query->fetch_assoc()) {
+		if (empty($media_data['nome'])) {
+			$media_data['nome'] = $row['nome'];
+			$media_data['sinopse'] = $row['sinopse'];
+			$media_data['duracao'] = $row['duracao'];
+		}
+		if ($media_data['duracao'] < $row['duracao']) {
+			$media_data['duracao'] = $row['duracao'];
+		}
+
+		$media_data['medias'][] = [$row['link'], $row['numero'], $row['duracao']];
+	}
 }
 
 $conexao->close();
--- js/loadMediaInfo.js
+++ js/loadMediaInfo.js
@@ -2,7 +2,7 @@ function createEntry(link, id, serie, titulo) {
     let linha = document.createElement('tr');
     linha.innerHTML = `
         <td>${id}</td>
-        <td class='nome'>${(serie) ? titulo + ' episódio' + id: 'Filme ' + titulo}</td>
+        <td class='nome'>${(serie) ? 'Episódio ' + id + ' de ' + titulo: 'Filme ' + titulo}</td>
     `;
     linha.onclick = () => {
         window.open(link, '_blank');
@@ -26,18 +26,25 @@ fetch('../control/getMidiaInfo.php' + arg, {
     return response.json();
 })
 
-.then(data => {
-		console.log(data);
-    document.getElementById('titulo').innerHTML = data.nome;
-    document.getElementById('midia_descricao').innerHTML = document.getElementById('midia_descricao').innerHTML + data.sinopse;
-    document.getElementById('midia_duracao').innerHTML = '<b>' + ((data.duracao.includes(':')) ? 'Duração: ' + data.duracao : data.duracao + ' temporada(s)') + '</b>';
-    document.getElementById('midia_image').src = window.location.href.split('.com')[0] + '.com/img/posters/' + data.nome.toLowerCase().replace(/ /g, '_') + '.jpg';
-    var index = 0;
-    data.links.forEach((link) => {
-        index += 1;
-        createEntry(link, index, data.tipo === 'serie', data.nome);
-    });
-})
+	.then(data => {
+		document.getElementById('title').innerHTML = data.nome;
+		document.getElementById('midia_descricao').innerHTML = document.getElementById('midia_descricao').innerHTML + data.sinopse;
+		document.getElementById('midia_duracao').innerHTML = '<b>' + ((data.duracao.includes(':')) ? 'Duração: ' + data.duracao : data.duracao + ' temporada(s)') + '</b>';
+		document.getElementById('midia_image').src = window.location.href.split('/page')[0] + '/img/posters/' + data.nome.toLowerCase().replace(/ /g, '_') + '.jpg'; // '.split(".com")' para url e '.split("/page")'
+		let temporadaPosta = 0;
+		if (data.medias[0][2] != 0) {
+			data.medias.sort((m1, m2) => m1[2] - m2[2]);
+		}
+		data.medias.forEach((media) => {
+			if (media[2] > temporadaPosta) {
+				let linha = document.createElement('tr');
+				linha.innerHTML = `<td colspan='2' class='divisao'>${media[2]} Temporada</td>`;
+				document.getElementById('corpo').appendChild(linha);
+				temporadaPosta = media[2];
+			}
+			createEntry(media[0], media[1], data.tipo === 'serie', data.nome);
+		});
+	})
 
 .catch(erro => {
     console.error("Erro: ", erro);
